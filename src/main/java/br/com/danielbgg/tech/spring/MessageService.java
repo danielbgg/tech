ce119aa763affed207249e4d1c28a1377e42a1e0
kerdlix <danielbgg@gmail.com>
2015-04-16 08:44:58 -0300

    Melhorias

--- src/main/java/br/com/danielbgg/tech/spring/MessageService.java
+++
@@ -1,5 +0,0 @@
-package br.com.danielbgg.tech.spring;
-
-public interface MessageService {
-	String getMessage();
-}
--- src/main/java/br/com/danielbgg/tech/spring/helloworld/MessagePrinter.java
+++ src/main/java/br/com/danielbgg/tech/spring/helloworld/MessagePrinter.java
@@ -1,6 +1,7 @@
 package br.com.danielbgg.tech.spring.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
